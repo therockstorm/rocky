bd261012a6f8581b8a784895d707809a80b2c596
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-04-30 11:32:43 -0600

    Fix class -> className issue

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -6,7 +6,7 @@ const Heart = (props) => (
     focusable="false"
     data-prefix="fad"
     data-icon="heart"
-    class="svg-inline--fa fa-heart fa-w-16"
+    className="svg-inline--fa fa-heart fa-w-16"
     role="img"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 512 512"
