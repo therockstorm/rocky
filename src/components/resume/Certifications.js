7b4ad99b535e4c4226778451f486af992aa3c9b9
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-05-08 17:33:21 -0600

    Inline certs and positions

--- src/components/resume/Certifications.js
+++ src/components/resume/Certifications.js
@@ -2,12 +2,25 @@
 import React from "react"
 import { Flex, jsx, Styled } from "theme-ui"
 import ExternalLink from "../ExternalLink"
-import certs from "./certs"
 import Section from "./Section"
 
 export default () => (
   <Section title="Certifications">
-    {certs.map((c, idx) => (
+    {[
+      {
+        title: "AWS Certified Solutions Architect - Associate",
+        href:
+          "https://www.certmetrics.com/amazon/public/badge.aspx?i:1&t:c&d:2020-04-14&ci:AWS01371544",
+        start: "Apr 2020",
+        end: "Apr 2023",
+      },
+      {
+        title: "Triplebyte Certified Software Engineer",
+        href: "https://triplebyte.com/certificate/TFJ0QbG",
+        start: "Jan 2017",
+        end: "No Expiration",
+      },
+    ].map((c, idx) => (
       <Styled.div key={idx}>
         <Certification {...c} />
       </Styled.div>
