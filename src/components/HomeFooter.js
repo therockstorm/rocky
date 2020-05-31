cc9bd25bfbdb88cab46f7387463796a01aa40ad2
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-05-31 11:46:17 -0600

    Fix footer margins

--- src/components/HomeFooter.js
+++ src/components/HomeFooter.js
@@ -7,9 +7,9 @@ import LinkedIn from "./icons/LinkedIn"
 import Twitter from "./icons/Twitter"
 import Icon from "./Icon"
 
-const IconLink = ({ icon, ...other }) => (
+const IconLink = ({ icon, mr, ...other }) => (
   <ExternalLink {...other}>
-    <Icon sx={{ mr: 4 }}>{icon}</Icon>
+    <Icon sx={{ mr }}>{icon}</Icon>
   </ExternalLink>
 )
 
@@ -21,21 +21,25 @@ const HomeFooter = () => (
         aria-label="Link to Twitter Profile"
         href={"https://twitter.com/therockstorm"}
         icon={<Twitter />}
+        mr={4}
       />
       <IconLink
         aria-label="Link to Instagram Profile"
         href={"https://www.instagram.com/therockstorm/"}
         icon={<Instagram />}
+        mr={4}
       />
       <IconLink
         aria-label="Link to LinkedIn Profile"
         href={"https://www.linkedin.com/in/rockywarren"}
         icon={<LinkedIn />}
+        mr={4}
       />
       <IconLink
         icon={<GitHub />}
         aria-label="Link to GitHub Profile"
         href={"https://github.com/therockstorm"}
+        mr={0}
       />
     </Flex>
   </footer>
