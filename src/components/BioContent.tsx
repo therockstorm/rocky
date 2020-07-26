5fcbde1ba94549afce3765fca0d231dc9e2b00d9
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-07-26 09:05:13 -0600

    Update deps, add link to rockywarren.com

--- src/components/BioContent.tsx
+++ src/components/BioContent.tsx
@@ -2,12 +2,17 @@
 import { Link } from "gatsby"
 import { ReactElement } from "react"
 import { Flex, jsx, Styled } from "theme-ui"
+import ExternalLink from "../components/ExternalLink"
 
 const BioContent = (): ReactElement => (
   <Styled.div>
     <Styled.p sx={{ mb: 1 }}>Rocky Warren&#39;s blog.</Styled.p>
     <Styled.p sx={{ mb: 1 }}>
-      Principal Engineer and Architect at Vertex. I do other stuff too.
+      Principal Engineer and Architect at Vertex. I{" "}
+      <ExternalLink href={"https://www.rockywarren.com/"}>
+        do other stuff
+      </ExternalLink>{" "}
+      too.
     </Styled.p>
     <Flex sx={{ flexWrap: `wrap` }}>
       <Styled.p sx={{ mb: 1 }}>
