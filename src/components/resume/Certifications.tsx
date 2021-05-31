38676a21f54d3d0d1399c3aefab11c071da86f26
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-05-31 08:18:01 -0600

    Fix cert link

--- src/components/resume/Certifications.tsx
+++ src/components/resume/Certifications.tsx
@@ -10,7 +10,7 @@ const Certifications = (): ReactElement => (
       {
         title: "AWS Certified Solutions Architect - Associate",
         href:
-          "https://www.certmetrics.com/amazon/public/badge.aspx?i=1&t=c&d=2020-04-14&ci=AWS01371544",
+          "https://www.credly.com/badges/f08cd558-4917-437c-8f3c-4c997d80ddbf/public_url",
         start: "Apr 2020",
         end: "Apr 2023",
       },
