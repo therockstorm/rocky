cee91476872a9ccde632bc281f0a74a4f920671d
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-09-27 08:48:27 -0600

    Update deps

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function SearchBox({ refine }: Props): JSX.Element {
   return (
     <form className="flex items-center my-4" role="search">
-      <Search className="mr-2 h-6 w-6" />
+      <Search className="mr-3 h-6 w-6" />
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="search"
