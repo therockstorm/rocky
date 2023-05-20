1a2ee67bf7dc05b5fb28091fd45c64e6749979d5
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2023-05-20 14:23:32 -0600

    chore: upgrade deps

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ export function Footer() {
   return (
     <footer className="mt-32 print:hidden">
       <ContainerOuter>
-        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
+        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 text-zinc-800 dark:text-zinc-200 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-6 text-sm font-medium">
